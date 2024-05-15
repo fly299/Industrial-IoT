56298bef010b7cf2811a9d0e966a6bd93c6f497c
fly299 <ul0258040@edu.uni.lodz.pl>
2024-05-15 15:05:23 +0200

    connect to opc

---
+++ OpcAgent/OpcDevice.cs
@@ -0,0 +1,15 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+using Microsoft.Azure.Devices.Client;
+using System.Text;
+
+public class OpcDevice
+{
+    OpcClient client = new OpcClient(File.ReadAllLines($"../../../../Config.txt")[1]);
+
+    public void Start()
+    {
+        Console.WriteLine(File.ReadAllLines($"../../../../Config.txt")[1].ToString());
+        client.Connect();
+    }
+}
